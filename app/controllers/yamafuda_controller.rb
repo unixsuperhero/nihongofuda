d4148c89596b5de274ce4a9b1fc33a457c678772
jtoyota@saveology.com.heroku <jearsh@gmail.com>
2013-07-23 10:19:05 -0400

    implemented continuous scrolling

--- app/controllers/yamafuda_controller.rb
+++ app/controllers/yamafuda_controller.rb
@@ -1,9 +1,19 @@
 class YamafudaController < ApplicationController
   before_action :track_random_url, only: :random
 
+  expose(:page) { (params[:page] || 1).to_i }
+  expose(:page_offset) { (page - 1) * 10 }
+  expose(:yamafuda) { Yamafuda.find_by(name: params[:name]) }
+  expose(:fuda_page) { yamafuda.all_fuda.limit(10).offset(page_offset) }
+
+  expose(:random_yamafuda) { yamafuda || Yamafuda.usable.order('random()').first }
+  expose(:random_card) { random_yamafuda && Fuda.where(id: random_yamafuda.fuda_ids).order('random()').first }
+
   expose(:fuda) { yamafuda.fuda.find_by(id: params[:id]) }
-  expose(:yamafuda) { Yamafuda.find_by(name: params[:name]) || Yamafuda.usable.order('random()').first }
-  expose(:random_card) { yamafuda && Fuda.where(id: yamafuda.fuda_ids).order('random()').first }
+
+  def show
+    render layout: nil and return if request.xhr?
+  end
 
   private
 
