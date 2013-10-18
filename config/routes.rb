fad9c5a0fd4f7c80a8d8e8404d0af139f89ba9fe
Joshua Toyota <jearsh@gmail.com>
2013-10-18 10:22:05 -0400

    adds users table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails4Base::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  devise_for :users
+
   scope :kanji do
     get '/:literal' => 'kanji#show', as: :kanji
   end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,17 +1,16 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  include ApplicationHelper
   def test_to_kana
-    assert to_kana('a') == 'あ'
+    assert Romaji.to_kana('a') == 'あ'
   end
 
   def test_more_complex_conversion
-    assert to_kana('ittekimasu') == 'いってきます'
+    assert Romaji.to_kana('ittekimasu') == 'いってきます'
   end
 
   def test_n_by_itself
-    assert to_kana('konnichiha') == 'こんにちは'
+    assert Romaji.to_kana('konnichiha') == 'こんにちは'
   end
 
   # should write more tests.  I just can't think of a finite number of use
