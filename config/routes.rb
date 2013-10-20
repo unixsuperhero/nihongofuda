5ec647a76ad65c0010d40d0e98d4e5e022a0664d
Joshua Toyota <jearsh@gmail.com>
2013-10-20 19:29:20 -0400

    configures app to work with pow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails4Base::Application.routes.draw do
+Nihongofuda::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
